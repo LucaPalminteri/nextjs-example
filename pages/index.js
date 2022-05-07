a2d277afc7cdc079f77f57dfa7df5eeec27d7131
LucaPalminteri <lucapalminteri02@gmail.com>
2022-05-07 12:08:23 -0300

    trying to solve a git problem

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from 'next/head'; // this is a coment
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/dist/client/link';
