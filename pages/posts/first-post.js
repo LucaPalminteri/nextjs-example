df73665cdd997638bc0aa02bd9974532c2274365
LucaPalminteri <lucapalminteri02@gmail.com>
2022-05-07 12:02:36 -0300

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First page</title>
+      </Head>
+      <h1>First Post</h1>
+    </Layout>
+  );
+}
